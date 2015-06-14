3ef603645f0d71af47dfd4ba760ced466778a71f
jesusrodriguez <jesus@MacBook-Pro-de-Jesus.local>
2015-06-14 17:55:54 +0200

    random versión 2
    Se corrigen fallos respecto al anterior

--- random.js
+++ random.js
@@ -1,9 +1,14 @@
-+// Math.random() devuelve número aleatorio entre 0 y 1.
-+var numero = Math.random();
-+
-+if (numero <= 0.5){
-+  console.log('\n' + numero + ' MENOR que 0,5 \n');
-+}
-+else {
-+  console.log('\n' + numero + ' MAYOR que 0,5 \n');
-+}
+// Math.random() devuelve número aleatorio entre 0 y 1.
+var numero = Math.random();
+
+var str =' MAYOR que 0,5';
+
+if (numero <= 0.5){
+  console.log('\n' + numero + ' MENOR que 0,5 \n');
+}
+else {
+  console.log('\n' + numero + ' MAYOR que 0,5 \n');
+  str = ' MENOR que 0,5';
+}
+
+console.log('\n' + numero + str + '\n');
